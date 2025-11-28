0aa5a66bdc84e7b79f72b3a6a1049501a9c19bab
36000.eth <1982775886@qq.com>
2025-11-28 20:50:40 +0800

    feat: text test

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
-import { generateText } from 'ai';
+import { z } from 'zod';
+import { generateObject } from 'ai';
 import { createOpenRouter } from '@openrouter/ai-sdk-provider';
 
 const openrouter = createOpenRouter({
@@ -7,11 +8,19 @@ const openrouter = createOpenRouter({
 });
 
 async function main() {
-  const { text } = await generateText({
-    model: openrouter.chat('anthropic/claude-3.5-sonnet'),
-    prompt: 'What is OpenRouter?',
+  const { object } = await generateObject({
+    model: openrouter('anthropic/claude-sonnet-4.5'),
+    schema: z.object({
+      recipe: z.object({
+        name: z.string(),
+        ingredients: z.array(z.string()),
+        steps: z.array(z.string()),
+      }),
+    }),
+    prompt: 'Generate a lasagna recipe.',
   });
-  console.log(text);
+
+  console.log(JSON.stringify(object, null, 2));
 }
 
 main();
