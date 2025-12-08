555c235abb1e0bffb7fcf3797a6153177912db59
36000.eth <1982775886@qq.com>
2025-12-08 17:42:16 +0800

    feat: change prompt

--- commit.ts
+++ commit.ts
@@ -15,9 +15,10 @@ async function main() {
     'http://192.168.0.105:11434/api',
     'qwen3:14b',
     z.object({
-      commitMessage: z.string(),
+      type: z.enum(['feat', 'fix', 'chore']),
+      message: z.string(),
     }),
-    `以下是执行 git diff --staged 的结果：\n\n${diff}，请用中文写一个 commit message`,
+    `以下是执行 git diff --staged 的结果：\n\n${diff}，请确定 commit type 之后，用中文写一个 commit message，要求包含精准的关键词，有利于之后搜索`,
   );
   console.log(result);
 }
