4728c906d85549ce5567d780be6a22663cb76faa
36000.eth <1982775886@qq.com>
2025-12-08 20:11:10 +0800

    feat: 改进 commit 提示：处理空 diff 情况，增强用户体验

--- commit.ts
+++ commit.ts
@@ -13,12 +13,12 @@ async function main() {
   const diff = getStagedDiff();
   const result = await generateObject(
     'http://192.168.0.105:11434/api',
-    'qwen2.5:3b',
+    'gpt-oss:20b',
     z.object({
       type: z.enum(['feat', 'fix', 'chore']),
       message: z.string().max(128),
     }),
-    `以下是执行 git diff --staged 的结果：\n\n${diff}\n\n请确定 commit type 之后，用中文写一个 commit message，要求包含精准的关键词，有利于之后搜索，要求长度小于 100 个字符`,
+    `以下是执行 git diff --staged 的结果：\n【${diff || '空'}】\n\n请确定 commit type 之后，用中文写一个 commit message，要求包含精准的关键词，有利于之后搜索，要求长度小于 100 个字符`,
   );
   console.log(result);
 }
