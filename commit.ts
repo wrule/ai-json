b2d4e78fa72aaec3aec7e29ff4c9d8814f6b63e6
36000.eth <1982775886@qq.com>
2025-12-08 17:33:07 +0800

    feat: add commit.ts

---
+++ commit.ts
@@ -0,0 +1,25 @@
+import { execSync } from 'child_process';
+import { generateObject, z } from './ollama';
+
+export function getStagedDiff(): string {
+  const diff = execSync('git diff --staged', {
+    encoding: 'utf-8',
+    maxBuffer: 1024 * 1024 * 32,
+  });
+  return diff;
+}
+
+async function main() {
+  const diff = getStagedDiff();
+  const result = await generateObject(
+    'http://192.168.0.105:11434/api',
+    'qwen3:14b',
+    z.object({
+      commitMessage: z.string(),
+    }),
+    `以下是执行 git diff --staged 的结果：\n\n${diff}，请写一个 commit message`,
+  );
+  console.log(result);
+}
+
+main();
