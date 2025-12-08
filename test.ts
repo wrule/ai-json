abfaf4606e9bd62efbb88906b1a5f2bc93ec9eca
36000.eth <1982775886@qq.com>
2025-12-08 17:11:11 +0800

    feat: change ollama url

--- test.ts
+++ test.ts
@@ -9,7 +9,7 @@ async function main() {
     age: z.number(),
   }));
   const time = Date.now();
-  const result = await generateObjectByJSON('http://192.168.0.106:11434/api', 'qwen3:14b', json);
+  const result = await generateObjectByJSON('http://192.168.0.105:11434/api', 'qwen3:14b', json);
   console.log(result, Date.now() - time);
 }
 
